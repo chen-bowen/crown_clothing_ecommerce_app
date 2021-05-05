2aa110f22334f41fa343d4077837bba6c5b5183b
Bowen Chen <chenbowen184@gmail.com>
2021-05-05 12:28:50 -0700

    add signin page styling, custom button component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 import Header from "./components/header/header.component";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import SigninAndSignupPage from "./pages/signin-and-signup/signin-and-signup.component";
 
 function App() {
   return (
@@ -11,7 +12,8 @@ function App() {
       <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route componentpath="/shop" component={ShopPage} />
+        <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SigninAndSignupPage} />
       </Switch>
     </div>
   );
---
+++ src/pages/signin-and-signup/signin-and-signup.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import SignIn from "../../components/sign-in/sign-in.component";
+import "./signin-and-signup.styles.scss";
+
+const SigninAndSignupPage = () => (
+  <div className="signin-and-signup">
+    <SignIn />
+  </div>
+);
+
+export default SigninAndSignupPage;
